6dcce492025f086c1794eda8eb9d77f9238f152c
Jeromy <jeromyj@gmail.com>
2013-03-15 15:43:56 -0700

    updating simplifies, moved CalcTerm to its own file

---
+++ CalcTerm.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+type CalcTerm struct {
+	factor float64
+	vr *Variable
+	power Equatable
+}
+
+func (ct *CalcTerm) Value() float64 {
+	return ct.factor * math.Pow(ct.vr.Value(),ct.power.Value())
+}
+
+func (ct *CalcTerm) Print() string {
+	return fmt.Sprintf("%f%s^%s",ct.factor, ct.vr.Print(), ct.power.Print())
+}
+
+func (ct *CalcTerm) simple() bool {
+	return false
+}
+
+func (ct *CalcTerm) Integrate() *CalcTerm {
+	nct := CalcTerm{}
+	nct.factor = ct.factor / ct.power.Value()
+	fmt.Println("Caution, integrate doesnt quite work yet")
+	nct.power = ct.power //+ 1
+	return &nct
+}
+
+func (ct *CalcTerm) ContainsVar(v uint8) bool {
+	return ct.vr.C == v || ct.power.ContainsVar(v)
+}
--- types.go
+++ types.go
@@ -75,38 +75,10 @@ func (e *Equality) Print() string {
 	return fmt.Sprintf("%s = %s",e.left.Print(), e.right.Print())
 }
 
-type CalcTerm struct {
-	factor float64
-	vr *Variable
-	power Equatable
-}
-
-func (ct *CalcTerm) Value() float64 {
-	return ct.factor * math.Pow(ct.vr.Value(),ct.power.Value())
-}
-
-func (ct *CalcTerm) Print() string {
-	return fmt.Sprintf("%f%s^%s",ct.factor, ct.vr.Print(), ct.power.Print())
-}
-
-func (ct *CalcTerm) simple() bool {
-	return false
-}
-
-func (ct *CalcTerm) Integrate() *CalcTerm {
-	nct := CalcTerm{}
-	nct.factor = ct.factor / ct.power.Value()
-	fmt.Println("Caution, integrate doesnt quite work yet")
-	nct.power = ct.power //+ 1
-	return &nct
-}
-
-func (ct *CalcTerm) ContainsVar(v uint8) bool {
-	return ct.vr.C == v || ct.power.ContainsVar(v)
-}
 
 func Simplify(e Equatable) Equatable {
 	if e.simple() {
+		//anything like (4 - (5 * 6) + 3) that doesnt contain a variable
 		return &Constant{e.Value()}
 	} else {
 		t,ok := e.(*Term)
@@ -115,6 +87,7 @@ func Simplify(e Equatable) Equatable {
 			t.right = Simplify(t.right)
 			vl, okl := t.left.(*Variable)
 			vr, okr := t.right.(*Variable)
+			// any case of x*x, x/x, x+x, x-x
 			if okl && okr {
 				if vl.C == vr.C {
 					switch t.operator {
@@ -129,13 +102,13 @@ func Simplify(e Equatable) Equatable {
 					}
 				}
 			} else if okl {
-				r, cr := simplifyVars(vl, t.right, t.operator)
-				if cr {
+				r := simplifyVars(vl, t.right, t.operator)
+				if r != nil {
 					return r
 				}
 			} else if okr {
-				l, cl := simplifyVars(vr, t.left, t.operator)
-				if cl {
+				l := simplifyVars(vr, t.left, t.operator)
+				if l != nil {
 					return l
 				}
 			}
@@ -153,11 +126,11 @@ func Simplify(e Equatable) Equatable {
 //Also try to do:
 // (1 / x) -> (x ^ -1)
 // (3 / x) -> (3 * (x ^ -1))
-func simplifyVars(v *Variable, e Equatable, op int) (Equatable, bool) {
+func simplifyVars(v *Variable, e Equatable, op int) Equatable {
 	//check if 'e' is a term, otherwise we have nothing to do
 	t, ok := e.(*Term)
 	if !ok {
-		return nil, false
+		return nil
 	}
 
 	ttlv, ttokl := t.left.(*Variable)
@@ -166,26 +139,36 @@ func simplifyVars(v *Variable, e Equatable, op int) (Equatable, bool) {
 		if t.operator == OPow {
 			if op == OMul {
 				if t.right.simple() {
-					return &Term{v, &Constant{t.right.Value() + 1}, OPow}, true
+					return &Term{v, &Constant{t.right.Value() + 1}, OPow}
 				} else {
 					returnTerm := &Term{v, nil, OPow}
 					returnTerm.right = &Term{t.right, &Constant{1.0}, OAdd}
-					return returnTerm, true
+					return returnTerm
 				}
 			} else if op == ODiv {
 				if t.right.simple() {
-					return &Term{v, &Constant{1 - t.right.Value()}, OPow}, true
+					return &Term{v, &Constant{1 - t.right.Value()}, OPow}
 				} else {
 					returnTerm := &Term{v, nil, OPow}
 					returnTerm.right = &Term{&Constant{1.0}, t.right, OSub}
-					return returnTerm, true
+					return returnTerm
 				}
 			}
 		} else if t.operator == OMul {
 			//x * (x * 5) -> (5 * (x ^ 2))
-			//x / (x * 6) -> (1 / 6)
+			//TODO: Convert to CalcTerm 5x^2
+			if op == OMul {
+				return &Term{t.right, &Term{v, &Constant{2.0}, OPow}, OMul}
+			} else if op == ODiv {
+				//x / (x * 6) -> (1 / 6)
+				if t.right.simple() {
+					return &Constant{1 / t.right.Value()}
+				} else {
+					return &Term{&Constant{1.0}, t.right, ODiv}
+				}
+			}
 		}
 	}
 
-	return nil, false
+	return nil
 }
